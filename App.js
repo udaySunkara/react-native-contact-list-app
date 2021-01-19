67718431972e87fb8c70a19d08059d298ec424c2
Kumar <skuma947@LAMU02CP05APWD7.uhc.com>
2021-01-19 22:27:51 +0530

    Adding test cases for App.js

--- App.js
+++ App.js
@@ -14,44 +14,50 @@ import { toggleEdit } from './src/store/actions/contacts.action';
 
 const Stack = createStackNavigator();
 
+const StackNav = () => {
+  let isEditOn = false;
+  const isEditmodeOn = useSelector(state => { isEditOn = state.contacts.isEditable; return isEditOn } );
+  const contactList = useSelector(state => state.contacts.contactList );
+  const dispatch = useDispatch();
+
+  const enableEditView = () => {
+    dispatch(toggleEdit(!isEditOn));
+  }
+
+  const navigateToManageScreen = () => {
+    if (isEditmodeOn) return; navigate('Manage', { title: 'New contact' })
+  }
+
+  return <Stack.Navigator initialRouteName="Contacts">
+    <Stack.Screen
+      name="Contacts"
+      component={ContactList}
+      options={{
+        title: 'Contact list', headerRight: () => {
+          return <TouchableOpacity testId="newContact" onPress={() => { navigateToManageScreen(); }}>
+            <AntDesign name="pluscircle" size={24} color={isEditmodeOn ? '#cfcfcf' : '#00AA55'} />
+          </TouchableOpacity>
+        },
+        headerLeft: () => {
+        return <>{contactList && contactList.length > 0 ? <TouchableOpacity testId="selectContact" onPress={() => enableEditView()} style={styles.headerLeft}>{ !isEditmodeOn ? <Text testId="lblSelect" maxFontSizeMultiplier={1.5}>Select</Text>: <Text testId="lblUndo" maxFontSizeMultiplier={1.5}>Undo</Text>}</TouchableOpacity> : null}</>
+        },
+        headerRightContainerStyle: styles.headerRight,
+        headerTitleStyle: styles.headerTitle,
+        headerTitleAllowFontScaling: false
+      }}
+    />
+    <Stack.Screen
+      name="Manage"
+      component={ManageContact}
+      options={{ title: 'Edit contact', headerTitleStyle: styles.headerTitle, headerTitleAllowFontScaling: false }}
+    />
+  </Stack.Navigator>
+}
+
 export default function App() {
 
 
-  const StackNav = () => {
-    let isEditOn = false;
-    const isEditmodeOn = useSelector(state => { isEditOn = state.contacts.isEditable; return isEditOn } );
-    const contactList = useSelector(state => state.contacts.contactList );
-    const dispatch = useDispatch();
-
-    const enableEditView = () => {
-      dispatch(toggleEdit(!isEditOn));
-    }
-
-    return <Stack.Navigator initialRouteName="Contacts">
-      <Stack.Screen
-        name="Contacts"
-        component={ContactList}
-        options={{
-          title: 'Contact list', headerRight: () => {
-            return <TouchableOpacity onPress={() => { if (isEditmodeOn) return; navigate('Manage', { title: 'New contact' }) }}>
-              <AntDesign name="pluscircle" size={24} color={isEditmodeOn ? '#cfcfcf' : '#00AA55'} />
-            </TouchableOpacity>
-          },
-          headerLeft: () => {
-          return <>{contactList && contactList.length > 0 ? <TouchableOpacity onPress={() => enableEditView()} style={styles.headerLeft}>{ !isEditmodeOn ? <Text maxFontSizeMultiplier={1.5}>Select</Text>: <Text maxFontSizeMultiplier={1.5}>Undo</Text>}</TouchableOpacity> : null}</>
-          },
-          headerRightContainerStyle: styles.headerRight,
-          headerTitleStyle: styles.headerTitle,
-          headerTitleAllowFontScaling: false
-        }}
-      />
-      <Stack.Screen
-        name="Manage"
-        component={ManageContact}
-        options={{ title: 'Edit contact', headerTitleStyle: styles.headerTitle, headerTitleAllowFontScaling: false }}
-      />
-    </Stack.Navigator>
-  }
+  
 
   return (
     <Provider store={configureStore}>
--- App.test.js
+++ App.test.js
@@ -1,11 +1,81 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { useDispatch, useSelector } from 'react-redux';
+import { create, act } from 'react-test-renderer';
+import { Text } from 'react-native';
 
 import App from './App';
+import configureStore from './src/store/store';
+import { Provider } from 'react-redux';
+import * as rootNav from './src/utility/rootNavigation';
+
+
+jest.mock('react-redux', () => ({
+  ...jest.requireActual("react-redux"),
+  useSelector: jest.fn()
+}));
+let mockNavigate = null; 
 
 describe('<App />', () => {
-  it('has 1 child', () => {
-    const tree = renderer.create(<App />).toJSON();
+
+  beforeEach(() => {
+    mockNavigate = jest.spyOn(rootNav, 'navigate');
+    mockNavigate.mockImplementation(() => true);
+    useSelector.mockImplementation(callback => {
+      return callback({contacts: {isEditable: false, contactList: [], searchedContactList: []}});
+    });
+  });
+  afterEach(() => {
+    useSelector.mockClear();
+  });
+
+
+  it('App renders correctly', () => {
+    const tree = create(<Provider store={configureStore}><App /></Provider>).toJSON();
     expect(tree.children.length).toBe(1);
+  });
+
+  it('Test New contact on press event', () => {
+    const tree = create(<Provider store={configureStore}><App /></Provider>);
+    act(() => {
+      tree.root.findByProps({ 'testId': 'newContact' }).props.onPress()
+    })
+    expect(mockNavigate).toHaveBeenCalledWith('Manage', { title: 'New contact' });
+  });
+
+  it('Test whether select button is hidden if no contacts listed', () => {
+    
+    const tree = create(<Provider store={configureStore}><App /></Provider>);
+    let selectElmnt = null;
+      try {
+        selectElmnt = tree.root.findByProps({ 'testId': 'selectContact' });
+      } catch(e) {
+        expect(e).toEqual(Error('No instances found with props: {"testId":"selectContact"}'));
+      }
+  });
+
+  it('Test whether select button is shown if atleast one contact is listed', () => {
+    useSelector.mockImplementation(callback => {
+      return callback({contacts: {isEditable: false, contactList: [{id: 1, name: 'Abc', number: '', email: '', fax: '', company: ''}], searchedContactList: []}});
+    });
+
+    const tree = create(<Provider store={configureStore}><App /></Provider>);
+    const selectElmnt = tree.root.findByProps({ 'testId': 'selectContact' });
+    expect(selectElmnt.children.length).toBe(1);
+    tree.unmount();
+  });
+
+  it('Test whether undo label is shown if select on presess event is triggered', () => {
+    useSelector.mockImplementation(callback => {
+      return callback({contacts: {isEditable: true, contactList: [{id: 1, name: 'Abc', number: '', email: '', fax: '', company: ''}], searchedContactList: []}});
+    });
+
+    const tree = create(<App />);
+    act(() => {
+      tree.root.findByProps({ 'testId': 'selectContact' }).props.onPress();
+      
+    });
+
+    const undoElmnt = tree.root.findByProps({ 'testId': 'lblUndo' });
+    expect(undoElmnt.children.length).toBe(1);
   });
 });
